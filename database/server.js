6e7fed7241aaa2092fad562a7c48fd50f519b2b1
yasminaguilarx <yaguilar2001@gmail.com>
2023-06-18 15:13:02 -0500

    server side changes

--- database/server.js
+++ database/server.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 const { Pool } = require("pg");
+const cors = require("cors");
+const querystring = require("querystring");
+const cookieParser = require("cookie-parser");
 
 const dbstring = process.env.DATABASE_URL;
 const port = process.env.PORT;
@@ -11,18 +14,122 @@ const pool = new Pool({
   connectionString: dbstring,
 });
 
+const generateRandomString = function (length) {
+  const text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors());
+app.use(cookieParser());
 
-app.get("/music_search", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM music_search");
-    res.status(200).json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
+// Set up authorization flow
+const stateKey = "spotify_auth_state";
+const client_id = "cd7f7c4194ae475dbfaceb0b5962ccdf";
+const redirect_uri = "https://playlist-web-server.onrender.com/callback";
+const client_secret = "93dda00e1e4a44d8bed6d0c4a8edebc3";
+
+app.get("/login", async (req, res) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  const scopes = ["user-read-private", "user-read-email"];
+  const queryParams = new URLSearchParams({
+    response_type: "code",
+    client_id: client_id,
+    scope: scopes,
+    redirect_uri: redirect_uri,
+    state: state,
+  });
+  const authUrl = `https://accounts.spotify.com/authorize?${queryParams.toString()}`;
+  window.location.href = authUrl;
+});
+
+app.get("/callback", async (req, res) => {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.status(400).send("state_mismatch");
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: "https://accounts.spotify.com/api/token",
+      form: {
+        code: code,
+        redirect_uri: redirect_uri,
+        grant_type: "authorization_code",
+      },
+      headers: {
+        Authorization:
+          "Basic" +
+          new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+      },
+      json: true,
+    };
+
+    req.post(authOptions, (err, res, body) => {
+      if (!err && res.statusCode === 200) {
+        const access_token = body.access_token;
+        const refresh_token = body.refresh_token;
+
+        res.status(200).json({ access_token, refresh_token });
+      } else {
+        res.status(400).json("Invalid Token");
+      }
+    });
   }
 });
+
+// Function to handle the access token retrieval from the URL hash
+function getAccessTokenFromHash() {
+  const hashParams = {};
+  const hash = window.location.hash.substring(1);
+  const params = hash.split("&");
+  for (let i = 0; i < params.length; i++) {
+    const param = params[i].split("=");
+    hashParams[param[0]] = decodeURIComponent(param[1]);
+  }
+  return hashParams.access_token;
+}
+
+// Example API request to get user's profile
+function getUserProfile(accessToken) {
+  const apiUrl = "https://api.spotify.com/v1/me";
+
+  fetch(apiUrl, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the API response
+      console.log("User Profile:", data);
+    })
+    .catch((error) => {
+      // Handle error
+      console.error("Error:", error);
+    });
+}
+
+// Authorize the user and retrieve access token
+authorizeSpotify();
+
+// Once the user is redirected back to your application with the access token in the URL hash
+// Retrieve the access token from the hash
+const accessToken = getAccessTokenFromHash();
+
+// Use the access token to make API requests
+getUserProfile(accessToken);
 
 app.listen(port, () => {
   console.log(`Listening on port: ${port}`);
--- public/app.js
+++ public/app.js
@@ -1,1 +1,69 @@
-console.log("this is workin");
+// console.log("this is workin");
+
+// var client_id = "cd7f7c4194ae475dbfaceb0b5962ccdf";
+// var redirect_uri = "https://playlist-web-server.onrender.com/";
+
+// const express = require("express");
+// var app = express();
+
+// app.get("/login", function (req, res) {
+//   //   var state = generateRandomString(16);
+//   //   var scope = 'user-read-private user-read-email';
+
+//   res.redirect(
+//     "https://accounts.spotify.com/authorize?" +
+//       querystring.stringify({
+//         response_type: "code",
+//         client_id: client_id,
+//         scope: scope,
+//         redirect_uri: redirect_uri,
+//         state: state,
+//       })
+//   );
+// });
+
+// window.onSpotifyWebPlaybackSDKReady = () => {
+//     const token = '[My access token]';
+//     const player = new Spotify.Player({
+//         name: 'Web Playback SDK Quick Start Player',
+//         getOAuthToken: cb => { cb(token); },
+//         volume: 0.5
+//     });
+
+//     // Ready
+//     player.addListener('ready', ({ device_id }) => {
+//         console.log('Ready with Device ID', device_id);
+//     });
+
+//     // Not Ready
+//     player.addListener('not_ready', ({ device_id }) => {
+//         console.log('Device ID has gone offline', device_id);
+//     });
+
+//     player.addListener('initialization_error', ({ message }) => {
+//         console.error(message);
+//     });
+
+//     player.addListener('authentication_error', ({ message }) => {
+//         console.error(message);
+//     });
+
+//     player.addListener('account_error', ({ message }) => {
+//         console.error(message);
+//     });
+
+//     document.getElementById('togglePlay').onclick = function() {
+//       player.togglePlay();
+//     };
+
+//     player.connect();
+// }
+
+// start by gaining access to spotify?
+// async function fetchWebApi() {
+//   const res = await fetch(`https://api.spotify.com/authorize`);
+//   const data = await res.json();
+//   console.log(data);
+// }
+
+// `https://api.spotify.com/${endpoint}`
